d0016576ad0d48fe8a3b939c4a19d9e45fd2dd38
harits-adhi <harits.adhi.p@gmail.com>
2015-09-16 23:25:06 +0700

    Server v2.0
    Server now can send file according to cilent's request.
    Also added test client to see wether the server script works

--- ClientFileTest.java
+++ ClientFileTest.java
@@ -1,52 +1,80 @@
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
-public class ClientFileTest {
+public class Client implements Runnable {
 
-  public final static int SOCKET_PORT = 13267;      // change this accordingly
+BufferedReader br1, br2;
+PrintWriter pr1;
+Socket socket;
+Thread t1, t2;
+String in = "", out = "";
+public final static int SOCKET_PORT = 13267;      // change this accordingly
   public final static String SERVER = "localhost";  // use whatever server you want
   public final static String
-       FILE_TO_RECEIVED = "d:/progrin/file/a-download.txt";  // change this accordingly
+       FILE_TO_RECEIVED = "d:/progrin/file/downloaded.txt";  // change this accordingly
 
   public final static int FILE_SIZE = 6022386;
+  
+public Client() {
+    try {
+        t1 = new Thread(this);
+        t2 = new Thread(this);
+        socket = new Socket(SERVER, SOCKET_PORT);
+        t1.start();;
+        t2.start();
 
-  public static void main (String [] args ) throws IOException {
-    int bytesRead;
+    } catch (Exception e) {
+    }
+}
+
+public void run() {
+	int bytesRead;
     int current = 0;
     FileOutputStream fos = null;
     BufferedOutputStream bos = null;
     Socket sock = null;
+
     try {
-      sock = new Socket(SERVER, SOCKET_PORT);
-      System.out.println("Connecting...");
+        if (Thread.currentThread() == t2) {
+            do {
+                br1 = new BufferedReader(new InputStreamReader(System.in));
+                pr1 = new PrintWriter(socket.getOutputStream(), true);
+                in = br1.readLine();
+                pr1.println(in);
+            } while (!in.equals("END"));
+        } else {
+            
+                /*br2 = new BufferedReader(new   InputStreamReader(socket.getInputStream()));
+                out = br2.readLine();
+                System.out.println("Server says : " + out);*/
+				// receive file
+				  byte [] mybytearray  = new byte [FILE_SIZE];
+				  InputStream is = sock.getInputStream();
+				  fos = new FileOutputStream(FILE_TO_RECEIVED);
+				  bos = new BufferedOutputStream(fos);
+				  bytesRead = is.read(mybytearray,0,mybytearray.length);
+				  current = bytesRead;
 
-      // receive file
-      byte [] mybytearray  = new byte [FILE_SIZE];
-      InputStream is = sock.getInputStream();
-      fos = new FileOutputStream(FILE_TO_RECEIVED);
-      bos = new BufferedOutputStream(fos);
-      bytesRead = is.read(mybytearray,0,mybytearray.length);
-      current = bytesRead;
+				  do {
+					 bytesRead =
+						is.read(mybytearray, current, (mybytearray.length-current));
+					 if(bytesRead >= 0) current += bytesRead;
+				  } while(bytesRead > -1);
 
-      do {
-         bytesRead =
-            is.read(mybytearray, current, (mybytearray.length-current));
-         if(bytesRead >= 0) current += bytesRead;
-      } while(bytesRead > -1);
-
-      bos.write(mybytearray, 0 , current);
-      bos.flush();
-      System.out.println("File " + FILE_TO_RECEIVED
-          + " downloaded (" + current + " bytes read)");
+				  bos.write(mybytearray, 0 , current);
+				  bos.flush();
+				  System.out.println("File " + FILE_TO_RECEIVED
+					  + " downloaded (" + current + " bytes read)");
+            
+        }
+    } catch (Exception e) {
     }
-    finally {
-      if (fos != null) fos.close();
-      if (bos != null) bos.close();
-      if (sock != null) sock.close();
-    }
-  }
 
-}
+ }
+
+ public static void main(String[] args) {
+     new Client();
+ }
+ }
+ 
+ 
--- Server.java
+++ Server.java
@@ -1,17 +1,18 @@
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
-  public final static int SOCKET_PORT = 13267;  // change this accordingly
-  public final static String FILE_TO_SEND = "d:/progrin/file/a.txt";  // change this accordingly
+  public final static int SOCKET_PORT = 13267;  // you may change this
+  public final static String FILE_TO_SEND_1 = "d:/progrin/file/a.txt";  // you may change this
+  public final static String FILE_TO_SEND_2 = "d:/progrin/file/b.txt";
+  public final static String FILE_TO_SEND_3 = "d:/progrin/file/c.txt";
 
   public static void main (String [] args ) throws IOException {
+	BufferedReader br1, br2;
+	PrintWriter pr1;
+	String pgn = "List of files : \n 1. a.txt\n 2. b.txt\n 3. c.txt\n";
     FileInputStream fis = null;
     BufferedInputStream bis = null;
     OutputStream os = null;
@@ -24,17 +25,52 @@ public class Server {
         try {
           sock = servsock.accept();
           System.out.println("Accepted connection : " + sock);
-          // send file
-          File myFile = new File (FILE_TO_SEND);
-          byte [] mybytearray  = new byte [(int)myFile.length()];
-          fis = new FileInputStream(myFile);
-          bis = new BufferedInputStream(fis);
-          bis.read(mybytearray,0,mybytearray.length);
-          os = sock.getOutputStream();
-          System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
-          os.write(mybytearray,0,mybytearray.length);
-          os.flush();
-          System.out.println("Done.");
+		  br1 = new BufferedReader(new InputStreamReader(System.in));
+          pr1 = new PrintWriter(sock.getOutputStream(), true);
+          pr1.println(pgn);
+		  System.out.println("List of files : \n 1. a.txt\n 2. b.txt\n 3. c.txt\n");
+		  BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+		  String message = in.readLine();
+		  System.out.println("message recieve : " + message);
+		  if(message.equals("1")) {
+			  // send file
+			  File myFile = new File (FILE_TO_SEND_1);
+			  byte [] mybytearray  = new byte [(int)myFile.length()];
+			  fis = new FileInputStream(myFile);
+			  bis = new BufferedInputStream(fis);
+			  bis.read(mybytearray,0,mybytearray.length);
+			  os = sock.getOutputStream();
+			  System.out.println("Sending " + FILE_TO_SEND_1 + "(" + mybytearray.length + " bytes)");
+			  os.write(mybytearray,0,mybytearray.length);
+			  os.flush();
+			  System.out.println("Done.");
+		  }
+		  else if(message.equals("2")) {
+			  // send file
+			  File myFile = new File (FILE_TO_SEND_2);
+			  byte [] mybytearray  = new byte [(int)myFile.length()];
+			  fis = new FileInputStream(myFile);
+			  bis = new BufferedInputStream(fis);
+			  bis.read(mybytearray,0,mybytearray.length);
+			  os = sock.getOutputStream();
+			  System.out.println("Sending " + FILE_TO_SEND_2 + "(" + mybytearray.length + " bytes)");
+			  os.write(mybytearray,0,mybytearray.length);
+			  os.flush();
+			  System.out.println("Done.");
+		  }
+		  else if(message.equals("3")) {
+			  // send file
+			  File myFile = new File (FILE_TO_SEND_3);
+			  byte [] mybytearray  = new byte [(int)myFile.length()];
+			  fis = new FileInputStream(myFile);
+			  bis = new BufferedInputStream(fis);
+			  bis.read(mybytearray,0,mybytearray.length);
+			  os = sock.getOutputStream();
+			  System.out.println("Sending " + FILE_TO_SEND_3 + "(" + mybytearray.length + " bytes)");
+			  os.write(mybytearray,0,mybytearray.length);
+			  os.flush();
+			  System.out.println("Done.");
+		  }
         }
         finally {
           if (bis != null) bis.close();
