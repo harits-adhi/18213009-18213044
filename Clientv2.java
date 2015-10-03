5dcc0e102066a1bec58489652bc139ee89a87a8f
harits-adhi <harits.adhi.p@gmail.com>
2015-10-03 18:24:57 +0700

    Update Clientv2.java

--- Clientv2.java
+++ Clientv2.java
@@ -3,80 +3,60 @@ Pemberian Tugas : 15 September 2015
 import java.io.*;
 import java.net.*;
 
-public class Client implements Runnable {
+class Client{
+    public static void main(String argv[]) throws Exception
+ {
+  boolean isExit = false;
+  Socket clientSocket = new Socket("localhost", 13267);
+  String sentence;
+  String modifiedSentence;
+  System.out.println("List of files : \n 1. a.txt\n 2. b.txt\n 3. c.txt\n");
 
-BufferedReader br1, br2;
-PrintWriter pr1;
-Socket socket;
-Thread t1, t2;
-String in = "", out = "";
-public final static int SOCKET_PORT = 13267;      // change this accordingly
-  public final static String SERVER = "localhost";  // use whatever server you want
-  public final static String
-       FILE_TO_RECEIVED = "D:/progin/Client/downloaded.txt";  // change this accordingly
-
-  public final static int FILE_SIZE = 6022386;
   
-public Client() {
-    try {
-        t1 = new Thread(this);
-        t2 = new Thread(this);
-        socket = new Socket(SERVER, SOCKET_PORT);
-        t1.start();;
-        t2.start();
-		System.out.println("List of files : \n 1. a.txt\n 2. b.txt\n 3. c.txt\n");
-
-    } catch (Exception e) {
-    }
+  while(!(isExit)) 
+   {  
+  BufferedReader inFromUser = new BufferedReader(
+     new InputStreamReader(System.in));
+  DataOutputStream outToServer = new DataOutputStream(
+     clientSocket.getOutputStream());
+  BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+	 
+  sentence = inFromUser.readLine();
+  outToServer.writeBytes(sentence+"\n");
+  modifiedSentence = inFromServer.readLine();
+  System.out.println("FROM SERVER: File has been downloaded"); 
+	if(sentence.equals("1")){
+	byte[] mybytearray = new byte[1024];
+    InputStream is = clientSocket.getInputStream();
+    FileOutputStream fos = new FileOutputStream("fileSent1.txt");// nama file yg diterima dari server pada client
+    BufferedOutputStream bos = new BufferedOutputStream(fos);
+    int bytesRead = is.read(mybytearray, 0, mybytearray.length);
+    bos.write(mybytearray, 0, bytesRead);
+    bos.close();
+	sentence = "0";
+	
+	}
+	else if(sentence.equals("2")){
+		byte[] mybytearray = new byte[1024];
+		InputStream is = clientSocket.getInputStream();
+		FileOutputStream fos = new FileOutputStream("fileSent2.txt");// nama file yang diterima dari server pada client
+		BufferedOutputStream bos = new BufferedOutputStream(fos);
+		int bytesRead = is.read(mybytearray, 0, mybytearray.length);
+		bos.write(mybytearray, 0, bytesRead);
+		bos.close();
+		sentence = "0";
+	}
+	else if(sentence.equals("3")){
+		byte[] mybytearray = new byte[1024];
+		InputStream is = clientSocket.getInputStream();
+		FileOutputStream fos = new FileOutputStream("fileSent3.txt");// nama file yang diterima dari server pada client
+		BufferedOutputStream bos = new BufferedOutputStream(fos);
+		int bytesRead = is.read(mybytearray, 0, mybytearray.length);
+		bos.write(mybytearray, 0, bytesRead);
+		bos.close();
+		sentence = "0";
+	}
+	}
+  clientSocket.close();
+ }
 }
-
-public void run() {
-	int bytesRead;
-    int current = 0;
-    FileOutputStream fos = null;
-    BufferedOutputStream bos = null;
-    Socket sock = null;
-
-    try {
-        if (Thread.currentThread() == t2) {
-            do {
-                br1 = new BufferedReader(new InputStreamReader(System.in));
-                pr1 = new PrintWriter(socket.getOutputStream(), true);
-                in = br1.readLine();
-                pr1.println(in);
-            } while (!in.equals("END"));
-        } else {
-            
-                /*br2 = new BufferedReader(new   InputStreamReader(socket.getInputStream()));
-                out = br2.readLine();
-                System.out.println("Server says : " + out);*/
-				// receive file
-				  
-				  byte [] mybytearray  = new byte [FILE_SIZE];
-				  InputStream is = sock.getInputStream();
-				  fos = new FileOutputStream(FILE_TO_RECEIVED);
-				  bos = new BufferedOutputStream(fos);
-				  bytesRead = is.read(mybytearray,0,mybytearray.length);
-				  current = bytesRead;
-
-				  do {
-					 bytesRead =
-						is.read(mybytearray, current, (mybytearray.length-current));
-					 if(bytesRead >= 0) current += bytesRead;
-				  } while(bytesRead > -1);
-
-				  bos.write(mybytearray, 0 , current);
-				  bos.flush();
-				  System.out.println("File " + FILE_TO_RECEIVED
-					  + " downloaded (" + current + " bytes read)");
-            
-        }
-    } catch (Exception e) {
-    }
-
- }
-
- public static void main(String[] args) {
-     new Client();
- }
- }
